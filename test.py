3267069a3f1b371e93d2f16e962857dd8d10e448
Steven Phan <steven.phan009@gmail.com>
2021-09-08 00:08:52 -0700

    added todo lists and checkout flag

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ PATH = "D:\VS Code\Scraper\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 # driver.get("https://drsquatch.com/collections/bar-soaps");
-driver.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402")
-# driver.get("https://www.bestbuy.com/site/bose-headphones-700-wireless-noise-cancelling-over-the-ear-headphones-triple-black/6332173.p?skuId=6332173")
+# driver.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402") #3060ti
+driver.get("https://www.bestbuy.com/site/bose-headphones-700-wireless-noise-cancelling-over-the-ear-headphones-triple-black/6332173.p?skuId=6332173")
 # driver.get("https://techwithtim.net")
 print(driver.title)
 chrome_options = Options() 
@@ -37,16 +37,26 @@ def bestBuyAddToCart():
 
             # Check out the item
             # checkout = driver.find_element_by_xpath('//*[@id="cartApp"]/div[2]/div[1]/div/div[1]/div[1]/section[2]/div/div/div[4]/div/div[1]/button')
-            checkout = driver.find_element_by_class_name('btn-primary')
+            # checkout = driver.find_element_by_class_name('btn-primary')
+            checkout = driver.find_element_by_css_selector('.btn.btn-lg.btn-block.btn-primary')
             checkout.click()
             # item is added to cart and is ready to check out
             buyButton = True
             sleep(2)
+    return buyButton
 
 def main():
     while (1): #infinite loop
         # check best buy site to see item is in stock
-        bestBuyAddToCart()
+        checkOut = bestBuyAddToCart()
+
+        if checkOut == True: 
+            # TODO: need to be able to fill out checkout forms as user or guest
+            # TODO: add inputs for dummy credit card info
+            # TODO: be able to check multiple sites
+            # TODO: maybe spoof IP
+            pass
+        
         sleep(1) #add break point to pause
 
 
